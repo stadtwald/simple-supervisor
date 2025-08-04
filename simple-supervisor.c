76299cd088fce8bb47087347b43c69082a83ed9c
William Demchick <william.demchick@gmail.com>
2025-08-04 21:59:47 +1200

    move configuration to header file

--- simple-supervisor.c
+++ simple-supervisor.c
@@ -41,47 +41,10 @@ struct child_configuration {
     int is_startup_check;
 };
 
+#include "config.h"
+
 #define PHASE_CHECK 0
 #define PHASE_NORMAL 1
-
-/*** BEGIN CONFIGURATION ***/
-
-// this length includes a terminating line feed
-#define MAX_LINE_LENGTH 120
-
-// in seconds
-#define SHUTDOWN_TIMEOUT 10
-
-#define CHILDREN_COUNT 3
-
-const struct child_configuration child_configuration[CHILDREN_COUNT] = {
-    {
-        .command = { "/bin/sh", "-c", "while true; do sleep 5; echo 'hello'; done", NULL },
-        .name = "SLEEPER",
-        .receives_sigusr1 = 0,
-        .receives_sigusr2 = 0,
-        .termination_signal = SIGTERM,
-        .is_startup_check = 0
-    },
-    {
-        .command = { "/usr/bin/echo", "check done!", NULL },
-        .name = "CHECK",
-        .receives_sigusr1 = 0,
-        .receives_sigusr2 = 0,
-        .termination_signal = SIGTERM,
-        .is_startup_check = 1
-    },
-    {
-        .command = { "/usr/bin/sh", "-c", "echo doing check...; sleep 6", NULL },
-        .name = "CHECK2",
-        .receives_sigusr1 = 0,
-        .receives_sigusr2 = 0,
-        .termination_signal = SIGTERM,
-        .is_startup_check = 1
-    }
-};
-
-/*** END CONFIGURATION ***/
 
 struct buffer {
     char buffer[MAX_LINE_LENGTH + 1];
